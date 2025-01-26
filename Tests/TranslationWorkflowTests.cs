2ac07034ddd0a2d04d7ef9fe0297ffcba6801afb
Josh Freitas <tabjfr@rwwa.com.au>
2025-01-27 02:25:40 +0800

    Move manual trans

--- Tests/TranslationWorkflowTests.cs
+++ Tests/TranslationWorkflowTests.cs
@@ -14,7 +14,7 @@ public class TranslationWorkflowTests
     public async Task TranslateLines()
     {
         await TranslationService.TranslateViaLlmAsync(workingDirectory, false);
-        await PackageFinalTranslation();
+        //await PackageFinalTranslation();
     }
 
     [Fact]
