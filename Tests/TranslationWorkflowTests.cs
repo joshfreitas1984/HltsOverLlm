41398830afd1614f7c479a89ecd2d07485295cac
Josh Freitas <tabjfr@rwwa.com.au>
2025-02-16 07:06:24 +0800

    Before Doing punctuation

--- Tests/TranslationWorkflowTests.cs
+++ Tests/TranslationWorkflowTests.cs
@@ -261,8 +261,8 @@ public class TranslationWorkflowTests
         }
 
         // Characters
-        if ((split.Text.Contains("!") && !split.Translated.Contains("!"))
-            || (split.Text.Contains("?") && !split.Translated.Contains("?")))
+        if (((split.Text.Contains("!") || split.Text.Contains("！")) && !split.Translated.Contains("!"))
+            || ((split.Text.Contains("?") || split.Text.Contains("？")) && !split.Translated.Contains("?")))
         {
             Console.WriteLine($"Missing Punctuation {outputFile} Replaces: \n{split.Translated}");
             split.FlaggedForRetranslation = true;
